2983e25eeb5f61130184f7b050543cb9807256bd
NalandaMelo <nalandamelo28@gmail.com>
2026-04-08 16:28:27 -0300

    feat: criando uma pagina de autenticação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,18 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        loadChildren: () => import('./features/home/routes')
+        loadComponent: () => import('./core/layout/layout').then((m) => m.Layout),
+        children: [{
+            path : '',
+            loadChildren: () => import('./features/home/routes'),
+        }
+        ]
+
+
+    },
+    {
+        path: 'auth',
+        loadChildren: () => import('./core/auth/pages/routes')
 
 
     }
--- src/app/core/layout/components/header/header.ts
+++ src/app/core/layout/components/header/header.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatMenuModule} from '@angular/material/menu';
+
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
